72044b25cbdc9b5bfa2e9e537740acbfdcea0125
Jean Oliveira <jcwilbertt@gmail.com>
2023-08-29 21:16:47 +0100

    header content

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import './global.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import twitterLogo from './assets/logo-twitter.svg'
-import { House, MagnifyingGlass, Bell, EnvelopeSimple, ClipboardText, BookmarkSimple, User, DotsThreeCircle } from 'phosphor-react'
+import { House, MagnifyingGlass, Bell, EnvelopeSimple, ClipboardText, BookmarkSimple, User, DotsThreeCircle, Sparkle } from 'phosphor-react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
@@ -46,11 +46,31 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                 </nav>
 
                 <button className='new-tweet' type='button'>
-                    Tweet
+                    Postar
                 </button>
             </aside>
+
             <div className="content">
-                content
+                <main className="timeline">
+                    <div className="timeline-header">
+                        Página Inicial
+                        <Sparkle />
+                    </div>
+
+                    <form className="new-tweet-form">
+                        <label htmlFor="tweet">
+                            <img src="https://github.com/jhotas.png" alt="Foto de perfil" />
+                            <textarea id='tweet' placeholder='O que está acontecendo?' />
+                        </label>
+
+                        div.
+                        <button type='submit'>Postar</button>
+                    </form>
+                </main>
+            </div>
+
+            <div className="sidebar-2">
+                sidebar-2
             </div>
         </div>
     </React.StrictMode>
