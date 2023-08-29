a595f768e69fbcc8b4cb6cd1ec307a18003cf1e1
Jean Oliveira <jcwilbertt@gmail.com>
2023-08-29 18:26:01 +0100

    creating sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,31 @@
+import './global.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './global.css'
+import twitterLogo from './assets/logo-twitter.svg'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <div className='layout'>
-            <aside className='sidebar'></aside>
+            <aside className='sidebar'>
+                <img className='logo' src={twitterLogo} alt="Logo" />
+
+                <nav className='main-navigation'>
+                    <a href="">Página Inicial</a>
+                    <a href="">Explorar</a>
+                    <a href="">Notificações</a>
+                    <a href="">Mensagens</a>
+                    <a href="">Listas</a>
+                    <a href="">Itens Salvos</a>
+                    <a href="">Perfil</a>
+                    <a href="">Mais</a>
+                </nav>
+
+                <button className='new-tweet' type='button'>
+                    Tweet
+                </button>
+            </aside>
             <div className="content">
-                
+                content
             </div>
         </div>
     </React.StrictMode>
